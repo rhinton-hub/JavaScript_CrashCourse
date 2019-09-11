6db91cca84c2d19d4094d80b084cdf3910b2e2ed
rhinton-hub <53100244+rhinton-hub@users.noreply.github.com>
2019-09-11 15:52:55 -0400

    Added files via upload
    JavaScript practice files

---
+++ main.js
@@ -0,0 +1,26 @@
+// String, Numbers, Boolean, null, undefined
+
+const name = 'John';
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+
+// Concatenation
+console.log(`My name is ${name} and I am ${age}`);
+
+// Script Debug Example
+let x = 'Dont forget to bookmark this page!';
+
+alert(x);
+
+// String, Numbers, Boolean, null, undefined,
+
+const name = 'John'; // String
+const age = 30; // Number
+const isCool = true; //Boolean
+const x = null; //null
+const y = undefined; //undefined
+
+
+
+
